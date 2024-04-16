b1e8e47af613e34e26c7cb990f569f1aeba65ed4
Yongbeom-Kim <dernbu@gmail.com>
2024-04-17 03:41:51 +0800

    feat(backend): Files to integrate with 3rd party APIs

---
+++ tts/backend/src/api/aws_s3.py
@@ -0,0 +1,90 @@
+import logging
+import os
+from pathlib import Path
+from typing import List
+from dotenv import load_dotenv
+import boto3
+import botocore
+
+load_dotenv()
+
+bucket_name = os.environ['AWS_S3_AUDIO_BUCKET_NAME']
+url_expiration = int(os.environ['AWS_PRESIGNED_URL_EXPIRATION'])
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(bucket_name)
+
+
+def list_buckets() -> List[str]:
+    return [bucket.name for bucket in s3.buckets.all()]
+
+
+def upload_file(local_file_path: Path, bucket_file_key: str) -> List[str]:
+    assert local_file_path.exists(), f"File not found: {local_file_path}"
+    assert local_file_path.is_file(), f"Not a file: {local_file_path}"
+    with open(local_file_path, 'rb') as data:
+        bucket.put_object(Key=bucket_file_key, Body=data)
+
+
+def item_exists(key: str) -> bool:
+    try:
+        bucket.Object(key).load()
+    except botocore.exceptions.ClientError:
+        return False
+    return True
+
+
+def delete_item(key: str) -> None:
+    bucket.Object(key).delete()
+
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-presigned-urls.html
+def create_presigned_download_url(object_name, expiration=url_expiration):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+
+    try:
+        s3_client = boto3.client('s3')
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except botocore.exceptions.ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL
+    return response
+
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-presigned-urls.html
+def create_presigned_upload_url(object_name, expiration=url_expiration):
+    """Generate a presigned URL S3 POST request to upload a file
+
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Dictionary with the following keys:
+        url: URL to post to
+        fields: Dictionary of form fields and values to submit with the POST
+    :return: None if error.
+    """
+
+    # Generate a presigned S3 POST URL
+    try:
+        s3_client = boto3.client('s3')
+        response = s3_client.generate_presigned_post(bucket_name,
+                                                     object_name,
+                                                     Fields=None,
+                                                     Conditions=None,
+                                                     ExpiresIn=expiration)
+    except botocore.exceptions.ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL and required fields
+    return response
---
+++ tts/backend/src/api/runpod.py
@@ -0,0 +1,136 @@
+from typing import Dict, List, Literal, Tuple, TypedDict
+import os
+import requests
+
+Model = Literal["tiny", "base", "small", "medium", "large-v1", "large-v2"]
+JobStatus = Literal['IN_PROGRESS', 'COMPLETED', 'ERROR', 'IN_QUEUE']
+Transcript = List[TypedDict(
+    'TranscriptSegment', {'start': float, 'end': float, 'text': str})]
+
+RUNPOD_API_KEY = os.getenv("RUNPOD_API_KEY")
+
+
+def submit_audio(audio_request_response: requests.Response) -> Tuple[bool, str, str]:
+    """ Submit audio to Runpod whisper API for transcription.
+
+    Args:
+        audio_request_response (requests.Response): The response from the audio request.
+
+    Returns:
+        bool: Whether the audio was successfully submitted.
+        str: Resulting job ID if successful. (Empty string if failed.)
+        str: Reason for failure if any. (Empty string if successful.)
+    """
+    if audio_request_response.status_code != 200:
+        return False, '', audio_request_response.text
+    return True, audio_request_response.json()['id'], ''
+
+
+def get_task_status(result_request_response: requests.Response) -> JobStatus:
+    """Get the status of the task from the result request response.
+
+    Args:
+        result_request_response (requests.Response): The response from the result request.
+
+    Returns:
+        JobStatus: The status of the task.
+    """
+    # FIXME: Handle task failure.
+    if result_request_response.status_code != 200:
+        return 'ERROR'
+
+    match result_request_response.json()['status']:
+        case 'COMPLETED':
+            return 'COMPLETED'
+        case 'IN_PROGRESS':
+            return 'IN_PROGRESS'
+        case 'IN_QUEUE':
+            return 'IN_QUEUE'
+
+    raise ValueError(f'Unknown task status, {result_request_response.json()}')
+
+
+def get_transcription(result_request_response: requests.Response) -> Transcript | None:
+    """Get the transcription from the result request response.
+
+    Args:
+        result_request_response (requests.Response): The response from the result request.
+
+    Returns:
+        Transcript | None: The transcription of the audio if completed, else None.
+    """
+    if result_request_response.status_code != 200:
+        return None
+    if get_task_status(result_request_response) != 'COMPLETED':
+        return None
+
+    segments = result_request_response.json()['output']['segments']
+    transcript: List[Dict[str, int | float | str]] = [{
+        'start': s['start'],
+        'end': s['end'],
+        'text': s['text']}
+        for s in segments]
+
+    return transcript
+
+
+def submit_audio_request(wav_file_url: str, model_name: Model = 'base') -> requests.Response:
+    """Submit audio to Runpod whisper API for transcription.
+
+    Args:
+        wav_file_url (str): The download URL of the audio file (.wav) to be transcribed.
+        model_name (Model, optional): The model to be used for transcription. Defaults to 'base'.
+
+    Returns:
+        requests.Response: The response from the API.
+    """
+    url = "https://api.runpod.ai/v2/faster-whisper/run"
+    payload = {
+        "input": {
+            "audio": wav_file_url,
+            "model": model_name,
+            "transcription": "plain_text",
+            "translate": False,
+            "language": "en",
+            "temperature": 0,
+            "best_of": 5,
+            "beam_size": 5,
+            "patience": 1,
+            "suppress_tokens": "-1",
+            "condition_on_previous_text": False,
+            "temperature_increment_on_fallback": 0.2,
+            "compression_ratio_threshold": 2.4,
+            "logprob_threshold": -1,
+            "no_speech_threshold": 0.6,
+            "word_timestamps": False
+        },
+        "enable_vad": False
+    }
+
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": RUNPOD_API_KEY
+    }
+
+    return requests.post(url, json=payload, headers=headers)
+
+
+def submit_result_request(job_id: str) -> requests.Response:
+    """Submit a request to get the result of the transcription task.
+
+    Args:
+        job_id (str): The job ID of the transcription task.
+
+    Returns:
+        requests.Response: The response from the API.
+
+    """
+    url = f"https://api.runpod.ai/v2/faster-whisper/status/{job_id}"
+
+    headers = {
+        "accept": "application/json",
+        "authorization": RUNPOD_API_KEY
+    }
+
+    return requests.get(url, headers=headers)
