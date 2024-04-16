def29b0ed7c3a3f2dff68bfa013d54b10a70e6d3
Yongbeom-Kim <dernbu@gmail.com>
2024-04-17 03:46:12 +0800

    feat(backend): Add s3 cors configuration to terraform

--- tts/backend/terraform/s3.tf
+++ tts/backend/terraform/s3.tf
@@ -6,3 +6,13 @@ resource "aws_s3_bucket" "upload_bucket" {
     Environment = "Production"
   }
 }
+
+resource "aws_s3_bucket_cors_configuration" "example" {
+  bucket = aws_s3_bucket.upload_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["POST"]
+    allowed_origins = ["*"]
+  }
+}
