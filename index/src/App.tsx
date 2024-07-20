cc028acbfe1513ddb8ff7dc125f613424e003cbe
Yongbeom-Kim <dernbu@gmail.com>
2024-07-20 16:38:28 +0800

    feat(index): Setup prettier + husky pre-commit

--- index/src/App.tsx
+++ index/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import DarkModeButton from './components/DarkModeButton/DarkModeButton'
 
 function App() {
-
   return (
-    <div className='h-screen w-screen bg-gradient-to-tr dark:from-black dark:to-blue-950 from-white to-blue-100'>
+    <div className="h-screen w-screen bg-gradient-to-tr dark:from-black dark:to-blue-950 from-white to-blue-100">
       <DarkModeButton />
     </div>
   )
--- index/src/components/DarkModeButton/DarkModeButton.tsx
+++ index/src/components/DarkModeButton/DarkModeButton.tsx
@@ -1,52 +1,57 @@
-import { IconBulb, IconBulbFilled } from "@tabler/icons-react";
-import { useEffect, useState } from "react";
-import { isMobile } from "../../util/screen";
+import { IconBulb, IconBulbFilled } from '@tabler/icons-react'
+import { useEffect, useState } from 'react'
+import { isMobile } from '../../util/screen'
 
-type Theme = 'light' | 'dark';
+type Theme = 'light' | 'dark'
 
 export default function DarkModeButton() {
-    const [theme, setTheme] = useState<Theme>(getBrowserTheme());
-    const toggleTheme = () => {
-        setTheme(theme => theme === 'light' ? 'dark' : 'light');
-    }
+  const [theme, setTheme] = useState<Theme>(getBrowserTheme())
+  const toggleTheme = () => {
+    setTheme((theme) => (theme === 'light' ? 'dark' : 'light'))
+  }
 
-    const [isButtonHover, setButtonHover] = useState<boolean>(false);
+  const [isButtonHover, setButtonHover] = useState<boolean>(false)
 
-    useEffect(() => {
-        setBrowserTheme(theme);
-    }, [theme])
-    
-    return (
-        <div>
-            <button 
-                className="bg-transparent hover:bg-none outline-none hover:outline-none focus:outline-none border-transparent focus:border-transparent"
-                onClick={toggleTheme}
-                onMouseEnter={() => setButtonHover(true)}
-                onMouseLeave={() => setButtonHover(false)}>
-                {(!isMobile() && !isButtonHover) && <IconBulb {... theme == 'dark' && {color: "white"}} />}
-                {(isMobile() || isButtonHover) && <IconBulbFilled {... theme == 'dark' && {color: "white"}} />}
-            </button>
-        </div>
-    )
+  useEffect(() => {
+    setBrowserTheme(theme)
+  }, [theme])
+
+  return (
+    <div>
+      <button
+        className="bg-transparent hover:bg-none outline-none hover:outline-none focus:outline-none border-transparent focus:border-transparent"
+        onClick={toggleTheme}
+        onMouseEnter={() => setButtonHover(true)}
+        onMouseLeave={() => setButtonHover(false)}
+      >
+        {!isMobile() && !isButtonHover && (
+          <IconBulb {...(theme == 'dark' && { color: 'white' })} />
+        )}
+        {(isMobile() || isButtonHover) && (
+          <IconBulbFilled {...(theme == 'dark' && { color: 'white' })} />
+        )}
+      </button>
+    </div>
+  )
 }
 
 function getBrowserTheme(): Theme {
-    if (localStorage.getItem('theme') !== 'null') {
-        return localStorage.getItem('theme') as Theme;
-    }
-    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-        localStorage.setItem('theme', 'light');
-        return 'light';
-    }
-    localStorage.setItem('theme', 'dark');
-    return 'dark';
+  if (localStorage.getItem('theme') !== 'null') {
+    return localStorage.getItem('theme') as Theme
+  }
+  if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+    localStorage.setItem('theme', 'light')
+    return 'light'
+  }
+  localStorage.setItem('theme', 'dark')
+  return 'dark'
 }
 
 function setBrowserTheme(theme: Theme) {
-    localStorage.setItem('theme', theme);
-    if (theme === 'light') {
-        document.querySelector('html')?.classList.remove('dark');
-    } else {
-        document.querySelector('html')?.classList.add('dark');
-    }
+  localStorage.setItem('theme', theme)
+  if (theme === 'light') {
+    document.querySelector('html')?.classList.remove('dark')
+  } else {
+    document.querySelector('html')?.classList.add('dark')
+  }
 }
