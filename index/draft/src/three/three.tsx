63df45181f8abc0b284f5e4cdff624fdeb6e5c8b
Yongbeom-Kim <dernbu@gmail.com>
2024-07-23 00:06:26 +0800

    feat(index): Move around camera!

--- index/draft/src/three/three.tsx
+++ index/draft/src/three/three.tsx
@@ -1,8 +1,10 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import classNames from 'classnames'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import * as THREE from 'three'
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { PointerLockControls, PerspectiveCamera } from '@react-three/drei'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
+import { PointerLockControls as PointerLockControlsType } from 'three/addons/controls/PointerLockControls'
 
 type ThreeCanvasProps = React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLDivElement>,
@@ -10,30 +12,17 @@ type ThreeCanvasProps = React.DetailedHTMLProps<
 >
 
 export default function ThreeCanvas({ className, ...props }: ThreeCanvasProps) {
-  const cameraPosition = new THREE.Vector3(14, 5, 14)
-  const cameraOrbitPosition = new THREE.Vector3(0, 3, 0)
-  const cameraAngle = Math.atan2(
-    cameraPosition.y - cameraOrbitPosition.y,
-    Math.sqrt(
-      (cameraPosition.x - cameraOrbitPosition.x) ** 2 +
-        (cameraPosition.z - cameraOrbitPosition.z) ** 2
-    )
-  )
+  const gltf = useLoader(GLTFLoader, '/models/scene.gltf')
 
   return (
     <div className={classNames('w-screen h-screen', className)} {...props}>
       <Canvas shadows={true}>
-        <PerspectiveCamera
-          makeDefault
-          position={cameraPosition}
-          rotation={[0, 0, 1]}
+        <primitive
+          object={gltf.scene}
+          position={[0, 0, 0]}
+          children-0-castShadow
         />
-        <OrbitControls
-          maxPolarAngle={Math.PI / 2 - cameraAngle}
-          minPolarAngle={Math.PI / 2 - cameraAngle}
-          target={cameraOrbitPosition}
-          enablePan={false}
-        />
+        <Camera />
         <FloorMesh />
         <BoxMesh position={new THREE.Vector3(0, 1, 0)} size={2} />
         <BoxMesh position={new THREE.Vector3(3, 0.25, 0)} size={0.5} />
@@ -46,6 +35,97 @@ export default function ThreeCanvas({ className, ...props }: ThreeCanvasProps) {
         />
       </Canvas>
     </div>
+  )
+}
+
+function Camera() {
+  const controls = useRef<PointerLockControlsType | null>(null)
+
+  const cameraPosition = new THREE.Vector3(0, 2, 0)
+  const cameraOrbitPosition = new THREE.Vector3(0, 3, 0)
+
+  const [moveForward, setMoveForward] = useState(false)
+  const [moveBackward, setMoveBackward] = useState(false)
+  const [moveLeft, setMoveLeft] = useState(false)
+  const [moveRight, setMoveRight] = useState(false)
+
+  useFrame(() => {
+    if (!controls.current) {
+      return
+    }
+
+    const moveSpeed = 0.07
+
+    let x_delta = 0
+    let z_delta = 0
+
+    if (moveForward) {
+      x_delta += moveSpeed
+    }
+    if (moveBackward) {
+      x_delta -= moveSpeed
+    }
+    if (moveLeft) {
+      z_delta -= moveSpeed
+    }
+    if (moveRight) {
+      z_delta += moveSpeed
+    }
+
+    controls.current.moveForward(x_delta)
+    controls.current.moveRight(z_delta)
+  })
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    console.log(controls.current.getObject().position)
+    switch (event.key) {
+      case 'w':
+        setMoveForward(true)
+        break
+      case 's':
+        setMoveBackward(true)
+        break
+      case 'a':
+        setMoveLeft(true)
+        break
+      case 'd':
+        setMoveRight(true)
+        break
+    }
+  }
+
+  const onKeyUp = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case 'w':
+        setMoveForward(false)
+        break
+      case 's':
+        setMoveBackward(false)
+        break
+      case 'a':
+        setMoveLeft(false)
+        break
+      case 'd':
+        setMoveRight(false)
+        break
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyDown)
+    document.addEventListener('keyup', onKeyUp)
+
+    return () => {
+      document.removeEventListener('keydown', onKeyDown)
+      document.removeEventListener('keyup', onKeyUp)
+    }
+  }, [])
+
+  return (
+    <>
+      <PerspectiveCamera position={cameraPosition} />
+      <PointerLockControls ref={controls} />
+    </>
   )
 }
 
