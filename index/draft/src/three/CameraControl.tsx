7aabc1c36b97fafbb2bdb19da0669fc8abe554fd
Yongbeom-Kim <dernbu@gmail.com>
2024-07-23 04:29:13 +0800

    feat(index/draft): Walk around in room, with collision detection!

---
+++ index/draft/src/three/CameraControl.tsx
@@ -0,0 +1,166 @@
+import * as THREE from 'three'
+import { Box, PerspectiveCamera, PointerLockControls } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { useRef, useState, useEffect } from 'react'
+import { PointerLockControls as PointerLockControlsType } from 'three/addons/controls/PointerLockControls'
+
+export function Camera() {
+  const controls = useRef<PointerLockControlsType | null>(null)
+  // Box mesh that represents the player for collision detection
+  const playerMeshRef = useRef<THREE.Mesh>(null)
+
+  const cameraPosition = new THREE.Vector3(0, 2, 0)
+  const playerMeshRefOffset = new THREE.Vector3(0, -2, 0)
+
+  const keyboardMovementAnimateCallback = useKeyboardMovement({
+    controls,
+    cameraPosition,
+  })
+
+  useFrame(({ scene }) => {
+    if (controls.current === null) {
+      return
+    }
+    keyboardMovementAnimateCallback(controls, scene)
+
+    if (!playerMeshRef.current) {
+      return
+    }
+    playerMeshRef.current.position.addVectors(
+      controls.current.camera.position,
+      playerMeshRefOffset
+    )
+  })
+
+  return (
+    <>
+      <mesh position={[0, 0, 0]} ref={playerMeshRef}>
+        <Box args={[2, cameraPosition.y + 4, 2]} />
+        <meshBasicMaterial wireframe />
+      </mesh>
+      <PerspectiveCamera position={cameraPosition} />
+      <PointerLockControls ref={controls} />
+    </>
+  )
+}
+
+function useKeyboardMovement({
+  controls,
+  cameraPosition,
+}: {
+  controls: React.MutableRefObject<PointerLockControlsType | null>
+  cameraPosition: THREE.Vector3
+}) {
+  const moveSpeed = 0.07 // Speed of movement
+
+  const [moveForward, setMoveForward] = useState(false)
+  const [moveBackward, setMoveBackward] = useState(false)
+  const [moveLeft, setMoveLeft] = useState(false)
+  const [moveRight, setMoveRight] = useState(false)
+
+  const hasCollision = useCollisionDetection()
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case 'w':
+        setMoveForward(true)
+        break
+      case 's':
+        setMoveBackward(true)
+        break
+      case 'a':
+        setMoveLeft(true)
+        break
+      case 'd':
+        setMoveRight(true)
+        break
+    }
+  }
+
+  const onKeyUp = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case 'w':
+        setMoveForward(false)
+        break
+      case 's':
+        setMoveBackward(false)
+        break
+      case 'a':
+        setMoveLeft(false)
+        break
+      case 'd':
+        setMoveRight(false)
+        break
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyDown)
+    document.addEventListener('keyup', onKeyUp)
+    controls.current.camera.position.y = cameraPosition.y
+
+    new THREE.Raycaster()
+    return () => {
+      document.removeEventListener('keydown', onKeyDown)
+      document.removeEventListener('keyup', onKeyUp)
+    }
+  }, [])
+
+  const animate = (
+    controls: React.MutableRefObject<PointerLockControlsType | null>,
+    scene: THREE.Scene
+  ) => {
+    if (controls.current === null) {
+      return
+    }
+    const direction = new THREE.Vector3()
+    direction.set(0, 0, 0)
+
+    direction.z += Number(moveForward)
+    direction.z -= Number(moveBackward)
+    direction.x -= Number(moveLeft)
+    direction.x += Number(moveRight)
+
+    const intersectNormalVector = hasCollision(controls.current, scene)
+
+    if (intersectNormalVector.x == 0 && intersectNormalVector.z == 0) {
+      controls.current.moveForward(direction.z * moveSpeed)
+      controls.current.moveRight(direction.x * moveSpeed)
+    } else {
+      controls.current.camera.position.add(intersectNormalVector.negate())
+    }
+  }
+  return animate
+}
+
+function useCollisionDetection() {
+  //   const raycaster = new THREE.Raycaster()
+  const raycasters = [...Array(8).keys()].map(() => new THREE.Raycaster())
+  const direction = new THREE.Vector3()
+  const origin = new THREE.Vector3()
+
+  const hasCollision = (
+    controls: PointerLockControlsType,
+    scene: THREE.Scene
+  ) => {
+    origin.copy(controls.getObject().position)
+    direction.copy(controls.getDirection(new THREE.Vector3(0, 0, 0)).clone())
+    raycasters.forEach((raycaster, index) => {
+      direction.applyAxisAngle(
+        new THREE.Vector3(0, 1, 0),
+        (index * Math.PI) / 4
+      )
+      raycaster.set(origin, direction.clone())
+    })
+    const intersection = raycasters
+      .flatMap((ray) => ray.intersectObjects(scene.children)[0])
+      .filter((intersection) => intersection.distance < 1)[0]
+
+    if (intersection !== undefined) {
+      return intersection.face?.normal ?? new THREE.Vector3(0, 0, 0)
+    }
+    return new THREE.Vector3(0, 0, 0)
+  }
+
+  return hasCollision
+}
--- index/draft/src/three/three.tsx
+++ index/draft/src/three/three.tsx
@@ -1,10 +1,9 @@
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { Canvas, useLoader } from '@react-three/fiber'
 import classNames from 'classnames'
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
 import * as THREE from 'three'
-import { PointerLockControls, PerspectiveCamera } from '@react-three/drei'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
-import { PointerLockControls as PointerLockControlsType } from 'three/addons/controls/PointerLockControls'
+import { Camera } from './CameraControl'
 
 type ThreeCanvasProps = React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLDivElement>,
@@ -13,7 +12,6 @@ type ThreeCanvasProps = React.DetailedHTMLProps<
 
 export default function ThreeCanvas({ className, ...props }: ThreeCanvasProps) {
   const gltf = useLoader(GLTFLoader, '/models/scene.gltf')
-
   return (
     <div className={classNames('w-screen h-screen', className)} {...props}>
       <Canvas shadows={true}>
@@ -23,109 +21,14 @@ export default function ThreeCanvas({ className, ...props }: ThreeCanvasProps) {
           children-0-castShadow
         />
         <Camera />
-        <FloorMesh />
-        <BoxMesh position={new THREE.Vector3(0, 1, 0)} size={2} />
-        <BoxMesh position={new THREE.Vector3(3, 0.25, 0)} size={0.5} />
+        {/* <FloorMesh /> */}
+        {/* <BoxMesh position={new THREE.Vector3(0, 1, 0)} size={2} />
+        <BoxMesh position={new THREE.Vector3(3, 0.25, 0)} size={0.5} /> */}
         <ambientLight intensity={0.2} />
-        <spotLight intensity={7} position={[0, 5, 0]} />
-        <directionalLight
-          intensity={0.1}
-          position={[0, 4, -4]}
-          castShadow={true}
-        />
+        {/* <spotLight intensity={7} position={[0, 5, 0]} /> */}
+        <directionalLight intensity={0.1} position={[0, 4, -4]} />
       </Canvas>
     </div>
-  )
-}
-
-function Camera() {
-  const controls = useRef<PointerLockControlsType | null>(null)
-
-  const cameraPosition = new THREE.Vector3(0, 2, 0)
-  const cameraOrbitPosition = new THREE.Vector3(0, 3, 0)
-
-  const [moveForward, setMoveForward] = useState(false)
-  const [moveBackward, setMoveBackward] = useState(false)
-  const [moveLeft, setMoveLeft] = useState(false)
-  const [moveRight, setMoveRight] = useState(false)
-
-  useFrame(() => {
-    if (!controls.current) {
-      return
-    }
-
-    const moveSpeed = 0.07
-
-    let x_delta = 0
-    let z_delta = 0
-
-    if (moveForward) {
-      x_delta += moveSpeed
-    }
-    if (moveBackward) {
-      x_delta -= moveSpeed
-    }
-    if (moveLeft) {
-      z_delta -= moveSpeed
-    }
-    if (moveRight) {
-      z_delta += moveSpeed
-    }
-
-    controls.current.moveForward(x_delta)
-    controls.current.moveRight(z_delta)
-  })
-
-  const onKeyDown = (event: KeyboardEvent) => {
-    console.log(controls.current.getObject().position)
-    switch (event.key) {
-      case 'w':
-        setMoveForward(true)
-        break
-      case 's':
-        setMoveBackward(true)
-        break
-      case 'a':
-        setMoveLeft(true)
-        break
-      case 'd':
-        setMoveRight(true)
-        break
-    }
-  }
-
-  const onKeyUp = (event: KeyboardEvent) => {
-    switch (event.key) {
-      case 'w':
-        setMoveForward(false)
-        break
-      case 's':
-        setMoveBackward(false)
-        break
-      case 'a':
-        setMoveLeft(false)
-        break
-      case 'd':
-        setMoveRight(false)
-        break
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener('keydown', onKeyDown)
-    document.addEventListener('keyup', onKeyUp)
-
-    return () => {
-      document.removeEventListener('keydown', onKeyDown)
-      document.removeEventListener('keyup', onKeyUp)
-    }
-  }, [])
-
-  return (
-    <>
-      <PerspectiveCamera position={cameraPosition} />
-      <PointerLockControls ref={controls} />
-    </>
   )
 }
 
